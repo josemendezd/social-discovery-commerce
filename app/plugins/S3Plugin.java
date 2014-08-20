c6c5d6f21183feed3fc50a9610678730f8338b76
mindnervestech <jagbir.paul@gmail.com>
2014-08-20 19:58:56 +0530

    reveting S3 changes

--- app/plugins/S3Plugin.java
+++ app/plugins/S3Plugin.java
@@ -14,9 +14,11 @@ import play.Plugin;
 public class S3Plugin extends Plugin {
 
     public static final String AWS_S3_BUCKET = "aws.s3.bucket";
-    public static final String AWS_ACCESS_KEY = "AWSAccessKeyId";
-    public static final String AWS_SECRET_KEY = "AWSSecretKey";
-    public static final String BASE_URL_FORMAT="base.url.format";
+    /*public static final String AWS_ACCESS_KEY = "AWSAccessKeyId";
+    public static final String AWS_SECRET_KEY = "AWSSecretKey";*/
+    public static final String AWS_ACCESS_KEY = "aws.access.key";
+    public static final String AWS_SECRET_KEY = "aws.secret.key";
+	public static final String BASE_URL_FORMAT="base.url.format";
     private final Application application;
 
     public static AmazonS3 amazonS3;
@@ -31,8 +33,10 @@ public class S3Plugin extends Plugin {
 
     @Override
     public void onStart() {
-        String accessKey = System.getenv(AWS_ACCESS_KEY);//application.configuration().getString(AWS_ACCESS_KEY);
-        String secretKey = System.getenv(AWS_SECRET_KEY);//application.configuration().getString(AWS_SECRET_KEY);
+         String accessKey = application.configuration().getString(AWS_ACCESS_KEY);
+        String secretKey = application.configuration().getString(AWS_SECRET_KEY);
+		/*String accessKey = System.getenv(AWS_ACCESS_KEY);//application.configuration().getString(AWS_ACCESS_KEY);
+        String secretKey = System.getenv(AWS_SECRET_KEY);//application.configuration().getString(AWS_SECRET_KEY);*/
         
         System.out.println("accessKey:" + accessKey);
         System.out.println("secretKey:" + secretKey);
